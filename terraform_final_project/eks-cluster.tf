0cb7c824111f59a4e9bd4828dcf70f1b65c9ce0f
moshe.even <moshe.even@imperva.com>
2021-01-03 13:51:39 +0200

    starting eks with micky way

---
+++ terraform_final_project/eks-cluster.tf
@@ -0,0 +1,39 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = local.cluster_name
+  cluster_version = var.kubernetes_version
+  subnets         = module.vpc.private_subnets
+
+  tags = {
+    Environment = "training"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
+
+  vpc_id = module.vpc.vpc_id
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t3.medium"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "t3.large"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+    }
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
