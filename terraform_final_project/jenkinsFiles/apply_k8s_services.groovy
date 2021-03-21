26cf5c472206f3b0d249adc6bf8f313a826133e1
moshe.even <moshe.even@imperva.com>
2021-03-21 16:36:40 +0200

    jenkins job for installtion

--- terraform_final_project/jenkinsFiles/apply_k8s_services.groovy
+++ terraform_final_project/jenkinsFiles/apply_k8s_services.groovy
@@ -15,6 +15,16 @@ node('slave1'){
                     echo "Install consul"
                     kubectl create secret generic consul-gossip-encryption-key --from-literal=key="uDBV4e+LbFW3019YKPxIrg=="
                     helm install consul hashicorp/consul -f values_consul.yaml
+
+                    echo "apply changes for coredns"
+                    kubectl apply -f coredns.yaml
+
+                    echo "node exporter"
+                    helm repo add bitnami https://charts.bitnami.com/bitnami
+                    helm install k8s bitnami/node-exporter -f values_node-exporter.yaml  
+
+                    echo "filebeat"
+                    kubectl apply -f filebeat.yaml
                     '''
                 }   
         }
