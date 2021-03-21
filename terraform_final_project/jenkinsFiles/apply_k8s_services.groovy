6930d7bc317d81d060d996cc65e2a5ca4f942797
moshe.even <moshe.even@imperva.com>
2021-03-21 16:03:17 +0200

    jenkins job for installtion

---
+++ terraform_final_project/jenkinsFiles/apply_k8s_services.groovy
@@ -0,0 +1,15 @@
+// install filebaet, node-exporter, 
+node('slave1'){
+
+    stage('clone git repo'){
+        git branch: 'master', changelog: false, credentialsId: 'github', poll: false, url: 'git@github.com:mosheeven/final_project.git'
+    }
+
+    stage("Install Cosnul on Kubernetes") {
+            withCredentials([kubeconfigFile(credentialsId: 'kube', variable: 'KUBECONFIG')]) {
+            sh “”"
+            export KUBECONFIG=\${KUBECONFIG}
+            kubectl get pods
+            “”"
+    }
+}
